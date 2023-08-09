09c06a0b18fb754834fb067b40bcfb84a71e1959
Egor Sorokin <egorka_sorokin@mail.ru>
2023-08-09 15:22:08 +0300

    added main.py

--- main.py
+++ main.py
@@ -2,31 +2,31 @@ import sqlalchemy as sq
 from sqlalchemy.orm import declarative_base, relationship, sessionmaker
 from models import create_tables, Publisher, Book, Sale, Shop, Stock
 
-DSN = 'sqlite:///:memory:'
-# engine = sq.create_engine(DSN, echo=True)
+DSN = 'postgresql://postgres:postgres@localhost:5432/alchemy_db'
+
 engine = sq.create_engine(DSN)
 
 create_tables(engine)
 
-p1 = Publisher(name='Альпина', books = [
-    Book(title="6 минут: Ежедневник, который изменит вашу жизнь"),
-    Book(title="Тонкое искусство пофигизма: Парадоксальный способ жить счастливо"),
-    Book(title="Думай как математик: Как решать любые проблемы быстрее и эффективнее")])
-p2 = Publisher(name='Питер', books = [
-    Book(title="Python. Лучшие практики и инструменты"),
-    Book(title="Python. Чистый код для продолжающих"),
-    Book(title="Основы Data Science и Big Data. Python и наука о данных")])
-b1 = Book(title="Паттерны разработки на Python: TDD, DDD и событийно-ориентированная архитектура", publisher=p2)
-b2 = Book(title="Лабиринты Ехо", publisher=p2)
-b3 = Book(title="Зачем нужны эмоции", publisher=p1)
+p1 = Publisher(name='Анджей Сапковский', books = [
+    Book(title="Меч предназначения"),
+    Book(title="Последнее желание"),
+    Book(title="Кровь эльфов")])
+p2 = Publisher(name='Джордж Мартин', books = [
+    Book(title="Игра престолов"),
+    Book(title="Битва королей"),
+    Book(title="Буря мечей")])
+b1 = Book(title="Пир стервятников", publisher=p2)
+b2 = Book(title="Танец с драконами", publisher=p2)
+b3 = Book(title="Крещение огнём", publisher=p1)
 
-shop1 = Shop(name="Лабиринт")
+shop1 = Shop(name="Буквоед")
 stock1 = Stock(book=b1, shop=shop1, count=50)
 sale1 = Sale(price=250, stock=stock1, count=25)
 stock2 = Stock(book=b3, shop=shop1, count=50)
 sale2 = Sale(price=200, stock=stock2, count=15)
 
-shop2 = Shop(name="ЕвроБук")
+shop2 = Shop(name="Дом книги")
 stock3 = Stock(book=b2, shop=shop2, count=50)
 sale3 = Sale(price=250, stock=stock3, count=25)
 stock4 = Stock(book=b2, shop=shop2, count=50)
@@ -38,7 +38,7 @@ s.add_all([p1, p2, b1, b2, b3, shop1, shop2, stock1, stock2, stock3, stock4, sal
 s.commit()
 
 # x = input()
-x = 'Альпина'
+x = 'Анджей Сапковский'
 # print(s.query(Publisher).filter(sq.or_(Publisher.id==x, Publisher.name==x)).all()[0])
 
 p = s.query(Publisher).filter(sq.or_(Publisher.id==x, Publisher.name==x)).all()[0]
